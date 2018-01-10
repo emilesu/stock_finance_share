c6ed9d6ba33b494be354baa9daed075ff315f8a2
emilesu <emilesu@qq.com>
2018-01-11 00:26:15 +0800

    complete base data controller

--- app/controllers/admin/base_data_controller.rb
+++ app/controllers/admin/base_data_controller.rb
@@ -4,7 +4,26 @@ class Admin::BaseDataController < AdminController
     #code
   end
 
+  # 全局扫描更新 沪股 股票代码和名称
   def update_stock_symbol
-    #code
+
+    response = RestClient.get "http://web.juhe.cn:8080/finance/stock/shall", :params => { :key => JUHE_CONFIG["api_key"], :page => "2", :type => "4" }
+    data = JSON.parse(response.body)
+
+    data["result"]["data"].each do |s|
+      existing_stock = Stock.find_by_symbol( s["symbol"] )
+      if existing_stock.nil?
+        Stock.create!(
+          :symbol => s["symbol"],
+          :name => s["name"]
+        )
+      end
+    end
+    puts "更新完毕*******"
+
   end
+
+
+
+
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+
+  # ---去掉‘sh’或’sz‘后的股票代码 symbol---
+  def easy_symbol
+    self.symbol[2..7]
+  end
+  
 end
