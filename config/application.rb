df5205e37ec38fb4fcb93754d634e4b323c105c5
emilesu <emilesu@qq.com>
2018-01-12 00:02:59 +0800

    add beijing zone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module StockFinanceShare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.i18n.default_locale = "zh-CN"
+    config.time_zone = "Beijing"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
