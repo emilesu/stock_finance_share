5ff3379d1f89916cdecc5d02a49e2a3178f545e1
emilesu <emilesu@qq.com>
2018-01-22 02:32:14 +0800

    add admin_stock_index page

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -1,2 +1,53 @@
 class Admin::StocksController < AdminController
+
+  def index
+    @stocks = Stock.all.order("symbol")
+  end
+
+  def show
+    @stock = Stock.find(params[:id])
+  end
+
+  def new
+    @stock = Stock.new
+  end
+
+  def create
+    @stock = Stcok.new(stock_params)
+
+    if @stock.save
+      redirect_to admin_stocks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @stock = Stock.find(params[:id])
+  end
+
+  def update
+    @stock = Stock.find(params[:id])
+
+    if @stock.update(stock_params)
+      redirect_to admin_stocks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @stock = Stock.find(params[:id])
+
+    @stock.destroy
+    redirect_to admin_stocks_path
+  end
+
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:symbol, :name, :lrb, :llb, :zcb, :fzb, :gdb, :industry)
+  end
+
 end
