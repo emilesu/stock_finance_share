274da64784b9459052a142300a05c62938ab25fd
emilesu <emilesu@qq.com>
2018-01-23 14:39:34 +0800

    complete Results quarterly data sorting script

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,5 +4,106 @@ class Stock < ApplicationRecord
   def easy_symbol
     self.symbol[2..7]
   end
-  
+
+
+  # -----------------------------------财报季度数据整理脚本-----------------------------------
+
+  # ---当前 利润表/lrb 财报季度，用于修正数据读取和表格的显示。安装最新财报发布季度来进行设置---
+  # ---第一季度：1   第二季度：2    第三季度：3    年报季度：0---
+  def quarter_lrb
+    col = JSON.parse(self.lrb)[0]
+    data = col.split(",")[2]
+    if data[6] == "3"
+      return 1
+    end
+    if data[6] == "6"
+      return 2
+    end
+    if data[6] == "9"
+      return 3
+    end
+    if data[6] == "2"
+      return 0
+    end
+  end
+
+  # ---当前 利润表/lrb 财报季度，用于修正数据读取和表格的显示。安装最新财报发布季度来进行设置---
+  # ---第一季度：1   第二季度：2    第三季度：3    年报季度：0---
+  def quarter_llb
+    col = JSON.parse(self.llb)[0]
+    data = col.split(",")[2]
+    if data[6] == "3"
+      return 1
+    end
+    if data[6] == "6"
+      return 2
+    end
+    if data[6] == "9"
+      return 3
+    end
+    if data[6] == "2"
+      return 0
+    end
+  end
+
+  # ---当前 利润表/lrb 财报季度，用于修正数据读取和表格的显示。安装最新财报发布季度来进行设置---
+  # ---第一季度：1   第二季度：2    第三季度：3    年报季度：0---
+  def quarter_zcb
+    col = JSON.parse(self.zcb)[0]
+    data = col.split(",")[2]
+    if data[6] == "3"
+      return 1
+    end
+    if data[6] == "6"
+      return 2
+    end
+    if data[6] == "9"
+      return 3
+    end
+    if data[6] == "2"
+      return 0
+    end
+  end
+
+  # ---当前 利润表/lrb 财报季度，用于修正数据读取和表格的显示。安装最新财报发布季度来进行设置---
+  # ---第一季度：1   第二季度：2    第三季度：3    年报季度：0---
+  def quarter_fzb
+    col = JSON.parse(self.fzb)[0]
+    data = col.split(",")[2]
+    if data[6] == "3"
+      return 1
+    end
+    if data[6] == "6"
+      return 2
+    end
+    if data[6] == "9"
+      return 3
+    end
+    if data[6] == "2"
+      return 0
+    end
+  end
+
+  # ---当前 利润表/lrb 财报季度，用于修正数据读取和表格的显示。安装最新财报发布季度来进行设置---
+  # ---第一季度：1   第二季度：2    第三季度：3    年报季度：0---
+  def quarter_gdb
+    col = JSON.parse(self.gdb)[0]
+    data = col.split(",")[2]
+    if data[6] == "3"
+      return 1
+    end
+    if data[6] == "6"
+      return 2
+    end
+    if data[6] == "9"
+      return 3
+    end
+    if data[6] == "2"
+      return 0
+    end
+  end
+
+
+
+
 end
