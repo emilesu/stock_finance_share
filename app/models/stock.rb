6f3dfdaf434ddf54a1575c44f2121796a0e77b65
emilesu <emilesu@qq.com>
2018-01-24 22:30:38 +0800

    complete cash_flow_adequancy_ratio function

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -126,14 +126,14 @@ class Stock < ApplicationRecord
       end
     end
     # 返回最近5年的年份
-    return year_all[0..4].reverse
+    return year_all
   end
 
-  # --- A1、现金流量比率（  >100%比较好 ）---
+  # --- A1-1、现金流量比率（  >100%比较好 ）---
   # =  营业活动现金流量llb27  /  流动负债fzb34
   def operating_cash_flow_ratio
     # 数据源
-    years = self.quarter_years
+    years = self.quarter_years[0..4].reverse
     col_llb = JSON.parse(self.llb).reverse
     col_fzb = JSON.parse(self.fzb).reverse
     # 数据提取 - 营业活动现金流量
@@ -156,6 +156,46 @@ class Stock < ApplicationRecord
     result = []
     (0..years.count-1).each do |i|
       m = col_llb_main[i].to_f / col_fzb_main[i].to_f * 100
+      result << eval(sprintf("%8.2f",m))
+    end
+    # 返回最近5年现金流量比率
+    return result
+  end
+
+  # --- A1-2、现金流量允当比率（  >100%比较好 ）---
+  # =  最近5年营业活动现金流量llb27  /  最近5年  (  资本支出llb35  + 存货增加额-llb77 + 现金股利llb50   )
+  def cash_flow_adequancy_ratio
+    # 数据源
+    years = self.quarter_years
+    col_llb = JSON.parse(self.llb).reverse
+    # 数据提取 - col_llb_main_1(营业活动现金流量)\col_llb_main_2(资本支出)\col_llb_main_3(存货增加额)\col_llb_main_4(现金股利)
+    col_llb_main_1 = []
+    col_llb_main_2 = []
+    col_llb_main_3 = []
+    col_llb_main_4 = []
+    col_llb.each do |i|
+      m = i.split(",")
+      if years.include?(m[2])
+        col_llb_main_1 << m[27]
+        col_llb_main_2 << m[35]
+        col_llb_main_3 << m[77]
+        col_llb_main_4 << m[50]
+      end
+    end
+    # 运算
+    c1 = []
+    c2 = []
+    c3 = []
+    c4 = []
+    (1..years.count-4).each do |i|
+      c1 << col_llb_main_1[-i].to_f + col_llb_main_1[-i-1].to_f + col_llb_main_1[-i-2].to_f + col_llb_main_1[-i-3].to_f + col_llb_main_1[-i-4].to_f
+      c2 << col_llb_main_2[-i].to_f + col_llb_main_2[-i-1].to_f + col_llb_main_2[-i-2].to_f + col_llb_main_2[-i-3].to_f + col_llb_main_2[-i-4].to_f
+      c3 << col_llb_main_3[-i].to_f + col_llb_main_3[-i-1].to_f + col_llb_main_3[-i-2].to_f + col_llb_main_3[-i-3].to_f + col_llb_main_3[-i-4].to_f
+      c4 << col_llb_main_4[-i].to_f + col_llb_main_4[-i-1].to_f + col_llb_main_4[-i-2].to_f + col_llb_main_4[-i-3].to_f + col_llb_main_4[-i-4].to_f
+    end
+    result = []
+    (0..c1.count-1).each do |i|
+      m = c1[i] * 100 / (c2[i] - c3[i] + c4[i])
       result << eval(sprintf("%8.2f",m))
     end
     # 返回最近5年现金流量比率
