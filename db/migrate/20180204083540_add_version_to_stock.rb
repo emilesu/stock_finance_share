984b6901c1557b9a05a0d0932281d6ca10a4a761
emilesu <emilesu@qq.com>
2018-02-04 17:50:16 +0800

    debug

--- db/migrate/20180204083540_add_version_to_stock.rb
+++ db/migrate/20180204083540_add_version_to_stock.rb
@@ -2,11 +2,4 @@ class AddVersionToStock < ActiveRecord::Migration[5.1]
   def change
     add_column :stocks, :version, :integer
   end
-  Stock.all.each do |s|
-    if s.lrb && s.llb && s.zcb != nil
-      s.update!(
-        :version => 1
-      )
-    end
-  end
 end
