60a15855b83caff541a36d78d73bdd31c0adc6d3
emilesu <emilesu@qq.com>
2018-02-28 20:18:16 +0800

    complete admin/course controller

---
+++ app/controllers/admin/courses_controller.rb
@@ -0,0 +1,39 @@
+class Admin::CoursesController < AdminController
+
+  def index
+    @courses = Course.all.order("created_at DESC")
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to admin_courses_path
+    else
+      rendew :new
+    else
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      redirect_to admin_courses_path
+    else
+      rendew :edit
+    else
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to admin_courses_path
+  end
+
+end
---
+++ test/controllers/admin/courses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::CoursesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
