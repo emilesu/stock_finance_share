802726010348189a57c072f7eed7498de5d9016d
emilesu <emilesu@qq.com>
2018-03-09 15:07:11 +0800

    add server to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 set :branch, "master"
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-server "47.92.166.199", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "www.holdle.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
