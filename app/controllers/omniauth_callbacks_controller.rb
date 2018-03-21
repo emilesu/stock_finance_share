5fd41be48728ed6b86942d3c92c9807f3eac64a9
emilesu <emilesu@qq.com>
2018-03-22 00:14:45 +0800

    text

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,13 +2,14 @@ class OmniauthCallbacksController < ApplicationController
 
 
   def wechat
-    @user = User.from_wechat(request.env["omniauth.auth"], current_user)
-    if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication
-    else
-        session["devise.user_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_path
-    end
+    # @user = User.from_wechat(request.env["omniauth.auth"], current_user)
+    # if @user.persisted?
+    #     sign_in_and_redirect @user, :event => :authentication
+    # else
+    #     session["devise.user_data"] = request.env["omniauth.auth"]
+    #     redirect_to new_user_registration_path
+    # end
+    puts "#{request.env["omniauth.auth"]}"
   end
 
 end
