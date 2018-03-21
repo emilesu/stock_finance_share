1eabaa567d2e4fffda449b7d0ee9fe97cfcf113d
emilesu <emilesu@qq.com>
2018-03-22 00:32:03 +0800

    text

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,14 +2,40 @@ class OmniauthCallbacksController < ApplicationController
 
 
   def wechat
-    # @user = User.from_wechat(request.env["omniauth.auth"], current_user)
-    # if @user.persisted?
-    #     sign_in_and_redirect @user, :event => :authentication
-    # else
-    #     session["devise.user_data"] = request.env["omniauth.auth"]
-    #     redirect_to new_user_registration_path
-    # end
-    puts "#{request.env["omniauth.auth"]}"
+    auth = request.env['omniauth.auth']
+    data = auth.info
+    identify = Identify.find_by(provider: access_token.provider, uid: access_token.uid)
+
+    if identify
+      @user = identify.user
+    else
+      user = User.create(
+        username: data.nickname,
+        opendid: auth.extra.raw_info.openid,
+        email: auth.extra.raw_info.openid + "@holdle.com",
+        avatar: data.headimgurl,
+        password: Devise.friendly_token[0,20]
+      )
+      identify = Identify.create(
+        provider: auth.provider,
+        uid: auth.uid,
+        user: user
+      )
+      @user = user
+    end
+
+    sign_in_and_redirect @user, :event => :authentication
+    redirect_to root
   end
+
+  # def wechat
+  #   @user = User.from_wechat(request.env["omniauth.auth"], current_user)
+  #   if @user.persisted?
+  #       sign_in_and_redirect @user, :event => :authentication
+  #   else
+  #       session["devise.user_data"] = request.env["omniauth.auth"]
+  #       redirect_to new_user_registration_path
+  #   end
+  # end
 
 end
