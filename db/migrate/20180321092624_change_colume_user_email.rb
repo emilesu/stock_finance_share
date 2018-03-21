df3f348fd2fb844e0053d20a11184c3c1067371d
emilesu <emilesu@qq.com>
2018-03-21 17:28:59 +0800

    change colume users email null true

---
+++ db/migrate/20180321092624_change_colume_user_email.rb
@@ -0,0 +1,5 @@
+class ChangeColumeUserEmail < ActiveRecord::Migration[5.1]
+  def change
+    change_column :users, :email, :string, :null => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321081806) do
+ActiveRecord::Schema.define(version: 20180321092624) do
 
   create_table "attentions", force: :cascade do |t|
     t.integer "user_id"
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 20180321081806) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
