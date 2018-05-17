74364ed5e6351c73434bb19e99589e56a248a298
emilesu <emilesu@qq.com>
2018-05-17 22:43:53 +0800

    debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517141625) do
+ActiveRecord::Schema.define(version: 20180517144116) do
 
   create_table "attentions", force: :cascade do |t|
     t.integer "user_id"
