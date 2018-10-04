f67ae1cce9e686ca0c16393d4b67b3e90c890f17
emilesu <emilesu@qq.com>
2018-10-04 17:40:34 +0800

    change sitemap host

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://localhost:3000"
+SitemapGenerator::Sitemap.default_host = "http://www.holdle.com/"
 
 SitemapGenerator::Sitemap.create do
 
