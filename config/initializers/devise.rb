b4f4cc70a944bdf6845f9f1ad9ef3f106e88d50e
emilesu <emilesu@qq.com>
2018-10-31 00:31:59 +0800

    weixin text

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,7 +277,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :wechat, KEY_CONFIG["open_weixin_app_id"], KEY_CONFIG["open_weixin_app_screct"], :authorize_params => {:scope => "snsapi_base"}
+  config.omniauth :wechat, KEY_CONFIG["open_weixin_app_id"], KEY_CONFIG["open_weixin_app_screct"]
 
   require 'omniauth-google-oauth2'
   config.omniauth :google_oauth2, KEY_CONFIG["GOOGLE_CLIENT_ID"], KEY_CONFIG["GOOGLE_CLIENT_SECRET"], {access_type: "offline", approval_prompt: ""}
