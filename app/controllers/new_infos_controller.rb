5211ab30e7e28aed442d10532e32cf073d93d156
emilesu <emilesu@qq.com>
2018-11-10 20:21:59 +0800

    completion new_info function and page

--- app/controllers/new_infos_controller.rb
+++ app/controllers/new_infos_controller.rb
@@ -1,2 +1,7 @@
 class NewInfosController < ApplicationController
+
+  def index
+    @new_infos = NewInfo.all.order("created_at desc").page(params[:page]).per(15)
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
   impressionist actions: [:index2, :about]
 
   def index2
-    @new_infos = NewInfo.all.order("created_at desc").page(params[:page]).per(6)
   end
 
   def about
