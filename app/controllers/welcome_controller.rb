70bb886a035f1c350fa518685ef2a70339568d63
emilesu <emilesu@qq.com>
2018-11-10 19:06:26 +0800

    complete new_info md&lg page show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   impressionist actions: [:index2, :about]
 
   def index2
+    @new_infos = NewInfo.all.order("created_at desc").page(params[:page]).per(6)
   end
 
   def about
