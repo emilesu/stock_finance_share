d86519618bca878c257526f766bacbc48a1e7088
emilesu <emilesu@qq.com>
2019-01-03 17:36:45 +0800

    wx_pay page text

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -33,7 +33,6 @@ class OmniauthCallbacksController < ApplicationController
     end
 
     sign_in_and_redirect @user, :event => :authentication
-    redirect_back(fallback_location: root_path)
   end
 
   # def wechat
@@ -113,7 +112,6 @@ class OmniauthCallbacksController < ApplicationController
     if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
         sign_in_and_redirect @user, :event => :authentication
-        redirect_back(fallback_location: root_path)
     else
         session["devise.user_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
