fea9860301d5d403689f5ef10dce12672e05e378
emilesu <emilesu@qq.com>
2019-03-06 03:08:56 +0800

    pramid text

--- app/models/us_stock.rb
+++ app/models/us_stock.rb
@@ -900,7 +900,7 @@ class UsStock < ApplicationRecord
     array = JSON.parse(self.static_data)[13]        # 导入数据
     num_array = array.delete_if {|x| x == 0 }         # 去除空数据
 
-    if num_array.size == 0 || (num_array.sum / num_array.size) == 0 || num_array.sum == 0 || num_array.min < 0 ||array.include?(0)
+    if num_array.size == 0 || (num_array.sum / num_array.size) == 0 || num_array.sum == 0 || num_array.min < 0 || array.min == 0
       rating = 0
     elsif num_array.min / (num_array.sum / num_array.size) < 0.3             # 排除掉最大值比最小值大3倍的极端情况
       rating = 0
