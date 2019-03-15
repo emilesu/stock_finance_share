410507f5efd67e3133818e449f6df0a8ecfe5163
emilesu <emilesu@qq.com>
2019-03-16 02:30:41 +0800

    wx_mp text

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -50,7 +50,7 @@ class OmniauthCallbacksController < ApplicationController
         email:  "#{i}@holdle.com",       # 因为devise 的缘故,邮箱暂做成随机
         avatar: data.headimgurl,
         password: i,                                              # 密码随机
-        motto: auth.fetch('extra')
+        motto: auth.fetch('info')
         # password_confirmation: i
       )
       identify = Identify.create(
