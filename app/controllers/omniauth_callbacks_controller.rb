0580137005d8fae3a74f46338dab1d9fecaef76a
emilesu <emilesu@qq.com>
2019-03-16 01:50:36 +0800

    wx_mp text

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -47,9 +47,8 @@ class OmniauthCallbacksController < ApplicationController
       i = Devise.friendly_token[0,20]
       user = User.create!(
         username: data.nickname.to_s + "_" + rand(36 ** 3).to_s(36),
-        # email:  "#{i}@holdle.com",       # 因为devise 的缘故,邮箱暂做成随机
-        email: auth.info,
-        avatar: data.headimgurl,
+        email:  "#{i}@holdle.com",       # 因为devise 的缘故,邮箱暂做成随机
+        avatar: data,
         password: i,                                              # 密码随机
         # password_confirmation: i
       )
