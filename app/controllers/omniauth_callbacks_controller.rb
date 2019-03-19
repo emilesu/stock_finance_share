846889e402406cc3eeea6b5dd699a782989d1e0c
emilesu <emilesu@qq.com>
2019-03-19 20:03:19 +0800

    login text

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -146,7 +146,7 @@ class OmniauthCallbacksController < ApplicationController
 
     if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
-        sign_in_and_redirect user_path(@user), :event => :authentication
+        sign_in_and_redirect @user, :event => :authentication
     else
         session["devise.user_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
